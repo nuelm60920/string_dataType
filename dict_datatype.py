8e83be65b5cbd3ac9a004e7aa989211d1b289c0f
Nuel <m60928@alunos.uevora.pt>
2026-04-20 16:28:30 +0100

    dictionary update

---
+++ dict_datatype.py
@@ -0,0 +1,104 @@
+my_dict = {'name':'Nuel', 'surname':'Edward','age':30, 'phone No': [68934983,9864778483]}
+print(type(my_dict))
+print(my_dict)
+
+friends = {'John':'080757578588','Mike':83664477848, 'Mary':8949494994}
+print(friends)
+
+name = my_dict['name']
+print(name)
+key_items = my_dict.keys()
+print(key_items)
+values = my_dict.values()
+print(values)
+items = my_dict.items()
+print(items)
+
+age=my_dict['age']
+
+my_dict['name'] = "Michael"
+print(my_dict)
+my_dict['height'] = 5.7
+print(my_dict)
+
+#del my_dict
+del my_dict['name']
+
+print('name' not in my_dict)
+#address = my_dict['address']
+address = my_dict.get('address','No valid address')
+print(address)
+
+print(len(my_dict))
+
+
+
+print(age)
+dict_function = dict(name = "James", phone_no =980938833,country='Nigeria')
+print(type(dict_function))
+print(dict_function)
+
+
+phones = {'samsung':"$2000", 'Iphone 14': '$25000','Readmi' :'$1500'}
+print(phones)
+print(phones.keys())
+key_list = list(phones.keys())
+print(key_list)
+key_values = list(phones.values())
+print(key_values)
+
+items = list(phones.items())
+print(items)
+
+
+university = {
+    'English Language': {
+        'teacher':'Nuel',
+        "stundents":30, 
+        "time": '9:30am',
+        'Phone_no':[9899938783, 98465662]},
+
+    'Programming': {
+        'teacher':'Mr John', 
+        "students":45, 
+        "time":"10:30am", 
+        "Phone_no": 977585773733},
+
+    "Maths":{
+        "teacher":"Mrs Mary", 
+        "students": 50, 
+        "time":'12:00pm', 
+        "phone_no": 574878484}
+}
+print(university)
+
+print('teacher' in university)
+print('English Language' in university)
+print('teacher' in university['English Language'])
+
+d ={'laptop':'mac pro 16'}
+e = dict(laptop ='mac pro 16')
+
+fruits = ['banana', 'orange','grape','apple']
+fruit_items = dict.fromkeys(fruits,50)
+print(fruit_items)
+
+d.update(fruit_items)
+print(d)
+
+d.update({'cashew':30, "mango":20,"strawberry":30})
+print(d)
+
+
+copy = d.copy()
+print(copy)
+#d.clear()
+print(d)
+
+d.pop('cashew')
+print(d)
+d.popitem()
+print(d)
+
+no_students = university['Programming']['students']
+print(no_students)
